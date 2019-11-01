2950239f2a75dc28cb1bdf046f6cb87cb7547e47
KAS <katsi@live.dk>
2019-11-01 17:07:10 +0100

    Deleted MainActivity (Kotlin) and added MainActivity (java), LoginActivity, activity_login, activity_main and drawable files

---
+++ app/src/main/java/com/example/campogrande/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.campogrande;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button joinNow, login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        joinNow = findViewById(R.id.join_now);
+        login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
