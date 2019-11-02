77fe625af0a85cb595e75339ec456e456f28f488
KAS <katsi@live.dk>
2019-11-02 18:52:36 +0100

    Added Firebase Authentication with email and password to RegisterActivity and added verification email to new users.

--- app/src/main/java/com/example/campogrande/MainActivity.java
+++ app/src/main/java/com/example/campogrande/MainActivity.java
@@ -7,9 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     private Button joinNow, login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,6 +27,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        joinNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity_new.class);
                 startActivity(intent);
             }
         });
