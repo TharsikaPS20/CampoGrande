774f5b5d3dc90046441e2ac483e9ef3e4740cc0f
KAS <katsi@live.dk>
2019-11-06 11:11:20 +0100

    Edited Loginactivity to add Firebase login based on authentication

--- app/src/main/java/com/example/campogrande/LoginActivity.java
+++ app/src/main/java/com/example/campogrande/LoginActivity.java
@@ -13,16 +13,11 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.example.campogrande.Model.Users;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
