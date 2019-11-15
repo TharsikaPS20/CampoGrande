6ad64d3ae518bbaca94329ca90d220093ef4fe44
Tharsika20 <44584392+Tharsika20@users.noreply.github.com>
2019-11-15 12:24:50 +0100

    working image slider

--- app/src/main/java/com/example/campogrande/PropertyDetailsActivity.java
+++ app/src/main/java/com/example/campogrande/PropertyDetailsActivity.java
@@ -1,14 +1,37 @@
 package com.example.campogrande;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.campogrande.Adapter.PropertyImagesAdapter;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PropertyDetailsActivity extends AppCompatActivity {
+
+    private ViewPager propertyImagesViewPager;
+    private TabLayout viewpagerIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_property_details);
+
+        propertyImagesViewPager = findViewById(R.id.property_images_viewpager);
+        viewpagerIndicator = findViewById(R.id.viewpager_indicator);
+
+        List<Integer> propertyImages = new ArrayList<>();
+        propertyImages.add(R.drawable.ic_campogrande);
+        propertyImages.add(R.drawable.background2);
+        propertyImages.add(R.drawable.backgroundgreen);
+        propertyImages.add(R.drawable.ic_menu_camera);
+
+        PropertyImagesAdapter propertyImagesAdapter = new PropertyImagesAdapter(propertyImages);
+        propertyImagesViewPager.setAdapter(propertyImagesAdapter);
+        viewpagerIndicator.setupWithViewPager(propertyImagesViewPager, true);
     }
 }
